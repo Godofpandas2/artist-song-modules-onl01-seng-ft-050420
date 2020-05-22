d20b820e122784619a3ce05450f76301119069aa
David Hutchison <dhutchis68516@gmail.com>
2020-05-22 15:28:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,37 +1,32 @@
+
 require 'pry'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/paramable'
+
 
 class Song
-  extend Memorable
-  attr_accessor :name
-  attr_reader :artist
+ extend Memorable
+ extend Findable
+ include Paramable
+attr_accessor :name
+attr_reader :artist
 
-  @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+@@songs = []
+def initialize
+  @@songs << self
+end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+def self.all
+  @@songs
+end
 
-  def self.all
-    @@songs
-  end
+def artist=(artist)
+  @artist = artist
+end
 
-  #def self.reset_all
-  #  self.all.clear
-  #end
-
-  #def self.count
-  #  self.all.count
-  #end
-
-  def artist=(artist)
-    @artist = artist
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+# def to_param
+#   name.downcase.gsub(' ', '-')
+# end
 end
